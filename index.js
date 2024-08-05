be0ce2bb41803244254a58d7e8315967c4f0bcf8
Pic Aurélia <pic.aurelia@gmail.com>
2024-08-05 11:08:17 +0200

    Add Poo Class and Finish exercise !

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require('./src/Fighter');
+
+const Heracles = new Fighter("Heracles",20,6);
+const Nemean = new Fighter("Nemean Lion", 11, 13);
+
+let round = 1;
+
+while (Heracles.isAlive() && Nemean.isAlive()) {
+    console.log(`Round #${round}`);
+    Heracles.fight(Nemean);
+    if (Nemean.isAlive()) {
+        Nemean.fight(Heracles);
+    }
+    round ++;
+}
+
+if (Heracles.life === 0 ) {
+    console.log(`💀 Heracles is dead`);
+    console.log(`🏆 Nemean win ! (♥ ${Nemean.life})`)
+} else if (Nemean.life === 0 ) {
+    console.log(`💀 Nemean is dead`);
+    console.log(`🏆 Heracles win ! (♥ ${Heracles.life})`)
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,32 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+class Fighter {
+    constructor (name, strength, dexterity) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+    }
+    fight(enemy) {
+
+        let damage = Math.floor(Math.random() * this.strength) + 1 ;
+
+        damage -= enemy.dexterity;
+        if (damage < 0) {
+            damage = 0;
+        }
+         
+        enemy.life -= damage;
+        if (enemy.life < 0 ) {
+            enemy.life = 0;
+        }
+        console.log(`${this.name} attacked ${enemy.name}, ${enemy.name} : ♥ ${enemy.life} left.`)
+    }
+
+    isAlive() {
+        return this.life > 0;
+    }
+}
+
+module.exports = Fighter
